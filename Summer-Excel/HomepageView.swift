fda51763e69b5f97196282d91e22602bfb37700b
SCHNASER, ETHAN <773017@ehs-s-232-28.local>
2017-12-18 11:38:41 -0600

    Deleted buttons to avoid SIGBART

--- Summer-Excel/HomepageView.swift
+++ Summer-Excel/HomepageView.swift
@@ -9,22 +9,4 @@
 import UIKit
 
 class HomepageView: UIViewController {
-    @IBOutlet weak var milesTextField: UITextField!
-    @IBOutlet weak var minutesTextField: UITextField!
-    @IBOutlet weak var notesTextField: UITextView!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}

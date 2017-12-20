3ac92c6a958e3c326feb9b1ce5b3960bb4e79006
MAXWELL MARSHALL <762928@ehs-s-232-27.local>
2017-12-20 12:31:31 -0600

    Worked on connecting log buttons to the code

--- Summer-Excel/HomepageView.swift
+++ Summer-Excel/HomepageView.swift
@@ -9,4 +9,19 @@
 import UIKit
 
 class HomepageView: UIViewController {
+    
+    @IBOutlet weak var mileInput: UITextField!
+    @IBOutlet weak var minuteInput: UITextField!
+    @IBOutlet weak var notesInput: UITextView!
+    @IBOutlet weak var attendanceInput: UISwitch!
+    
+    @IBAction func pressEnter(_ sender: Any) {
+        let theseMiles = Int(mileInput.text!)
+        let theseMinutes = Int(minuteInput.text!)
+        let theseNotes = notesInput.text
+        let attend = attendanceInput.isOn
+        
+        
+    }
+    
     }

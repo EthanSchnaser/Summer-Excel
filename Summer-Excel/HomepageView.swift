49e2da14165bd10418f1c7cb3a91f3fef6dc86ce
MAXWELL MARSHALL <762928@ehs-s-232-27.local>
2017-12-21 11:27:20 -0600

    worked on buttons

--- Summer-Excel/HomepageView.swift
+++ Summer-Excel/HomepageView.swift
@@ -16,12 +16,13 @@ class HomepageView: UIViewController {
     @IBOutlet weak var attendanceInput: UISwitch!
     
     @IBAction func pressEnter(_ sender: Any) {
-        let theseMiles = Int(mileInput.text!)
+        let theseMiles = Double(mileInput.text!)
         let theseMinutes = Int(minuteInput.text!)
         let theseNotes = notesInput.text
         let attend = attendanceInput.isOn
         
-        
+        //let thisTime = Time(min: Double)
+        //let thisWorkout = Workout(miles: theseMiles, timeE: Time, theDate: <#T##Date#>, words: <#T##String#>, attend: <#T##Bool#>)
     }
     
     }

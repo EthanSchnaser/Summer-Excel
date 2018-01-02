ce78646a9865b99a33ee66c0d7c35302f767e6b5
SCHNASER, ETHAN <773017@ehs-s-232-28.local>
2018-01-02 12:31:12 -0600

    Created sort function and attempted sorting popup

--- Summer-Excel/PersonalDataView.swift
+++ Summer-Excel/PersonalDataView.swift
@@ -8,18 +8,7 @@
 
 import UIKit
 
-class PersonalDataView: UIViewController,UITableViewDelegate, UITableViewDataSource {
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        return 1
-    }
-    
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default , reuseIdentifier: "cell")
-        cell.textLabel?.text = "hey"
-        return(cell)
-    }
 
-}
+
+class PersonalDataView: UIViewController{
+    }
--- Summer-Excel/TeamDataView.swift
+++ Summer-Excel/TeamDataView.swift
@@ -8,5 +8,60 @@
 
 import UIKit
 
-class TeamDataView: UIViewController {
+class TeamDataView: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return(theTeam.count)
     }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default , reuseIdentifier: "cell")
+        cell.textLabel?.text = theTeam[0].thisName
+        return(cell)
+    }
+    @IBAction func showPopUp(_ sender: Any) {
+        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbPopUpID") as! SortingPopUpViewController
+        self.addChildViewController(popOverVC)
+        popOverVC.view.frame = self.view.frame
+        self.view.addSubview(popOverVC.view)
+        popOverVC.didMove(toParentViewController: self)
+    }
+
+    func sortAlpha()
+    {
+        let Ethan = Athlete(name: "Ethan", grade: 12)
+        let Max = Athlete(name: "Max", grade: 12)
+        let Jake = Athlete(name: "Jake", grade: 12)
+        theTeam.append(Ethan)
+        theTeam.append(Max)
+        theTeam.append(Jake)
+        let length = theTeam.count
+        var tmpArray: Array = [""]
+        for i in stride(from: 0, to: length, by: 1)
+        {
+            tmpArray[i] = theTeam[i].thisName
+        }
+        tmpArray.sort()
+        for i in stride(from: 0, to: length, by: 1)
+        {
+            let store = theTeam[i].thisName
+            for i in stride(from: 0, to: length, by: 1)
+            {
+                if (store == tmpArray[i])
+                {
+                    let use = theTeam.remove(at: i)
+                    theTeam.insert(use, at:0)
+                }
+            }
+        }
+        for i in stride(from: 0, to: length, by: 1)
+        {
+        print(theTeam[i].thisName)
+        }
+        
+        
+    }
+    }
+

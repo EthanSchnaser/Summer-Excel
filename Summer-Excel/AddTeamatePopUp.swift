2e263b5f5aa88b0885631a709dda6a4fabc62467
MAXWELL MARSHALL <762928@ehs-s-232-27.local>
2018-01-02 11:28:27 -0600

    added error message

--- Summer-Excel/AddTeamatePopUp.swift
+++ Summer-Excel/AddTeamatePopUp.swift
@@ -29,32 +29,72 @@ class AddTeamatePopUp: UIViewController {
     @IBOutlet weak var gradeTen: UIButton!
     @IBOutlet weak var gradeEleven: UIButton!
     @IBOutlet weak var gradeTwelve: UIButton!
+    var alert = UIAlertView()
     
     @IBAction func pressNine(_ sender: Any) {
-        
+        gradeNine.isSelected = true
+        gradeTen.isSelected = false
+        gradeEleven.isSelected = false
+        gradeTwelve.isSelected = false
     }
     
     @IBAction func pressTen(_ sender: Any) {
+        gradeNine.isSelected = false
+        gradeTen.isSelected = true
+        gradeEleven.isSelected = false
+        gradeTwelve.isSelected = false
     }
     
     @IBAction func pressEleven(_ sender: Any) {
+        gradeNine.isSelected = false
+        gradeTen.isSelected = false
+        gradeEleven.isSelected = true
+        gradeTwelve.isSelected = false
     }
     
     @IBAction func pressTwelve(_ sender: Any) {
+        gradeNine.isSelected = false
+        gradeTen.isSelected = false
+        gradeEleven.isSelected = false
+        gradeTwelve.isSelected = true
     }
     
     
     @IBAction func addTeamate(_ sender: Any) {
         let athleteName = firstName.text! + " " + lastName.text!
+        var gradeSelected = 0
         
-        let newAthlete = Athlete(name: athleteName, grade: 10)
+        if (firstName.text == "") || (lastName.text == ""){
+            alert.title = "Please Enter a First and Last Name"
+            alert.addButton(withTitle: "Dismiss")
+            alert.show()
+            alert.dismiss(withClickedButtonIndex: 0, animated: true)
+            return
+        }
+        
+        if gradeNine.isSelected {
+            gradeSelected = 9
+        } else if gradeTen.isSelected {
+            gradeSelected = 10
+        } else if gradeEleven.isSelected {
+            gradeSelected = 11
+        } else if gradeTwelve.isSelected {
+            gradeSelected = 12
+        } else {
+            alert.title = "Please Select a Grade"
+            alert.addButton(withTitle: "Dismiss")
+            alert.show()
+            alert.dismiss(withClickedButtonIndex: 0, animated: true)
+            return
+        }
+        
+        let newAthlete = Athlete(name: athleteName, grade: gradeSelected)
         
         theTeam.append(newAthlete)
-        
-        print(theTeam[0].thisName)
         
         dismiss(animated: true, completion: nil)
         
     }
+    
 
 }

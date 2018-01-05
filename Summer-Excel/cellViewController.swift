ae96590d2d7dc689996dff5f5d6b9bb80cc90c69
Ethan Schnaser <ethanschnaser@Ethans-MacBook-Pro-2.local>
2018-01-05 12:16:22 -0600

    confused

---
+++ Summer-Excel/cellViewController.swift
@@ -0,0 +1,35 @@
+//
+//  cellViewController.swift
+//  Summer-Excel
+//
+//  Created by Ethan Schnaser on 1/4/18.
+//  Copyright © 2018 DIstrict 196. All rights reserved.
+//
+
+import UIKit
+
+class cellViewController: TeamDataView {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

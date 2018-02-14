cdc4b35915d4c3d0f7d9329545de391aa39c51e8
JACOB ABBOTT <755689@dh-a-entrance.district196.org>
2018-02-14 11:45:17 -0600

    The label is now fully functional and updates along with the UIdatePicker view

--- Summer-Excel/PersonalDataView.swift
+++ Summer-Excel/PersonalDataView.swift
@@ -16,22 +16,37 @@ class PersonalDataView: UIViewController{
    
     @IBOutlet weak var runDate: UILabel!
     
+    
+    @IBOutlet weak var milesButton: UITextField!
+    
     @IBOutlet weak var weeklyStatsPU: UIButton!
     
     @IBOutlet weak var totalStatsPU: UIButton!
     
+    @IBAction func changeDate(_ sender: AnyObject) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let strDate = dateFormatter.string(from: self.datePicker.date)
+        self.runDate.text = strDate
+        //everytime the date is changed in the datePicker, the label changes with it.
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         datePicker.datePickerMode = UIDatePickerMode.date
-        
+        //sets the display of the datePicker to mm dd yyyy
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/MM/yyyy"
-        let strDate = dateFormatter.string(from: datePicker.date)
+        let strDate = dateFormatter.string(from: self.datePicker.date)
         self.runDate.text = strDate
+        //sets the inital display date to the date datePicker is set to (Today)
+        
+        
+        
+        
         
     }
     
